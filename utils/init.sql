641c9b4abe0ff2960788bba43beb1aca1041e230
Rolo <diogo.rolo@academiadecodigo.org>
2023-11-16 15:01:55 +0000

    changes

--- utils/init.sql
+++ utils/init.sql
@@ -1,9 +1,3 @@
--- Create the database
-CREATE DATABASE todo;
-
--- Connect to the new database
-\c todo;
-
 -- Create tables and perform any other setup
 DROP TABLE IF EXISTS todo_items;
 CREATE TABLE IF NOT EXISTS todo_items (id SERIAL PRIMARY KEY, name VARCHAR(255), completed BOOLEAN DEFAULT false);
--- utils/remotemachinesetup.sh
+++ utils/remotemachinesetup.sh
@@ -10,25 +10,25 @@ sudo apt-get install -y postgresql postgresql-contrib git
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-# Clone the repository (replace REPO_URL with the actual URL)
-git clone REPO_URL
+# Clone the repository
+git clone https://github.com/RoloMiniCoder/deploymenttest.git
 
 # Navigate to the repository directory
-cd your_repository_directory
+cd deploymenttest
 
 # Install project dependencies
 npm install
 
-# Create PostgreSQL database and user (replace DB_NAME, DB_USER, and DB_PASSWORD)
-sudo -u postgres psql -c "CREATE DATABASE DB_NAME;"
-sudo -u postgres psql -c "CREATE USER DB_USER WITH PASSWORD 'DB_PASSWORD';"
-sudo -u postgres psql -c "ALTER ROLE DB_USER SET client_encoding TO 'utf8';"
-sudo -u postgres psql -c "ALTER ROLE DB_USER SET default_transaction_isolation TO 'read committed';"
-sudo -u postgres psql -c "ALTER ROLE DB_USER SET timezone TO 'UTC';"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE DB_NAME TO DB_USER;"
+# Create PostgreSQL database and user
+sudo -u postgres psql -c "CREATE DATABASE todo;"
+sudo -u postgres psql -c "CREATE USER myuser WITH PASSWORD 'secret';"
+sudo -u postgres psql -c "ALTER ROLE myuser SET client_encoding TO 'utf8';"
+sudo -u postgres psql -c "ALTER ROLE myuser SET default_transaction_isolation TO 'read committed';"
+sudo -u postgres psql -c "ALTER ROLE myuser SET timezone TO 'UTC';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE todo TO myuser;"
 
 # Run SQL script to create tables and schema (replace SCRIPT_NAME.sql)
-psql -h localhost -U DB_USER -d DB_NAME -a -f SCRIPT_NAME.sql
+psql -h localhost -U myuser -d todo -a -f ./init.sql
 
 # Run the Node.js application
 npm start
